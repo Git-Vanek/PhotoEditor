2f9bd742108cab332c50475e0289200e02ec3107
Ivan <ki16082005@gmail.com>
2024-11-09 13:13:46 +0300

    Удален экран первого запуска. Создан экран каждого запуска

--- app/src/main/java/com/example/photoeditor/MainActivity.kt
+++ app/src/main/java/com/example/photoeditor/MainActivity.kt
@@ -12,17 +12,12 @@ class MainActivity : AppCompatActivity() {
     val MY_SETTINGS: String = "my_settings"
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_PhotoEditor)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
         var sp: SharedPreferences =getSharedPreferences(MY_SETTINGS, Context.MODE_PRIVATE)
         var hasVisited:Boolean=sp.getBoolean("hasVisited", false)
-        if(!hasVisited) {
-            val cliclActivity4 = Intent(this, LaunchActivity::class.java)
-            startActivity(cliclActivity4)
-            var editor: SharedPreferences.Editor=sp.edit()
-            editor.putBoolean("hasVisited", true)
-            editor.apply()
-        }
+
     }
 }
