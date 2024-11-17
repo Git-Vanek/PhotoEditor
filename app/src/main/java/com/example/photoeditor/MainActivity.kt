af61e6da22c321c158bef1985411b7ebd51b29b0
Ivan <ki16082005@gmail.com>
2024-11-17 15:42:29 +0300

    Добавлены разментки фрагментов авторизации и регистрации.

--- app/src/main/java/com/example/photoeditor/MainActivity.kt
+++ app/src/main/java/com/example/photoeditor/MainActivity.kt
@@ -8,16 +8,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    var binding: ActivityMainBinding? = null
+    lateinit var binding: ActivityMainBinding
     val MY_SETTINGS: String = "my_settings"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_PhotoEditor)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
-        var sp: SharedPreferences =getSharedPreferences(MY_SETTINGS, Context.MODE_PRIVATE)
-
-
+        setContentView(binding.root)
+        val sighinFragment = SighinFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.card, sighinFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+        var sp: SharedPreferences = getSharedPreferences(MY_SETTINGS, Context.MODE_PRIVATE)
     }
 }
---
+++ app/src/main/java/com/example/photoeditor/SighinFragment.kt
@@ -0,0 +1,18 @@
+package com.example.photoeditor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SighinFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_sighin, container, false)
+    }
+}
