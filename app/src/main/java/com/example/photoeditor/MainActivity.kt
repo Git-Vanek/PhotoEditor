70fe32bdd4be325b48582504b4f07f6367d1d240
Ivan <ki16082005@gmail.com>
2024-11-17 18:16:33 +0300

    Добавлены разметка клавной активности и комментарии.

--- app/src/main/java/com/example/photoeditor/MainActivity.kt
+++ app/src/main/java/com/example/photoeditor/MainActivity.kt
@@ -7,23 +7,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    // Инициализация переменной для View Binding
     private lateinit var binding: ActivityMainBinding
-
-    // Ключ для SharedPreferences
     private val MY_SETTINGS: String = "my_settings"
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Установка темы для активности
         setTheme(R.style.Theme_PhotoEditor)
         super.onCreate(savedInstanceState)
-
-        // Инициализация View Binding
         binding = ActivityMainBinding.inflate(layoutInflater)
-        // Установка макета активности
         setContentView(binding.root)
-
-        // Получение экземпляра SharedPreferences
         val sp: SharedPreferences = getSharedPreferences(MY_SETTINGS, Context.MODE_PRIVATE)
+        // Настройка RecyclerView
+
+        binding.buttonAdd.setOnClickListener {
+            add()
+        }
+        binding.buttonDelete.setOnClickListener {
+            delete()
+        }
+    }
+
+    private fun add() {
+
+    }
+
+    private fun delete() {
+
     }
 }
